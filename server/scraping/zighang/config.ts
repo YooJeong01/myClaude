a076da04b592950aac7145593777ee9aedf34da5
황유정 <ujjh77@gmail.com>
2026-09-09 17:17:34 +0900

    [T45] 지행 API 경로 수정

--- server/scraping/zighang/config.ts
+++ server/scraping/zighang/config.ts
@@ -5,7 +5,7 @@
  */
 
 export const ZIGHANG_BASE_URL = 'https://zighang.com';
-export const ZIGHANG_API_BASE_URL = 'https://api.zighang.com/api';
-export const ZIGHANG_RECRUITMENTS_PATH = '/recruitments/v3';
+export const ZIGHANG_API_BASE_URL = 'https://api.zighang.com/api/';
+export const ZIGHANG_RECRUITMENTS_PATH = 'recruitments/v3';
 export const ZIGHANG_PAGE_SIZE = 20;
 export const MAX_PAGES = Number(process.env.SCRAPE_MAX_PAGES) || 10;
