89fcf7ee5201890b5853a994afe63d5aa0aef582
황유정 <ujjh77@gmail.com>
2026-09-13 21:00:13 +0900

    [fix] 디자인 시스템 리뷰 지적 반영

--- src/shared/ui/button.tsx
+++ src/shared/ui/button.tsx
@@ -74,7 +74,7 @@ const buttonVariants = cva({
       }
     },
     size: {
-      sm: { h: "8", minHeight: "8", px: "3" },
+      sm: { h: "8", px: "3" },
       default: { h: "10", px: "4" },
       lg: { h: "11", px: "5" },
       icon: { h: "10", minWidth: "10", p: "0", w: "10" }
